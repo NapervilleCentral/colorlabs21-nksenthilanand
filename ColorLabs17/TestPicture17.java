07f02808a094d531490802528fadf04935765f60
Nithela K Senthilanand <NKSenthilanand@stu.naperville203.org>
2025-11-10 11:25:39 -0600

    color lab

--- ColorLabs17/TestPicture17.java
+++ ColorLabs17/TestPicture17.java
@@ -29,7 +29,7 @@ public class TestPicture17
      
      //KNOW IT FOR THE TEST!!!!!
      //relative path
-     /**/
+     /*
       
      Picture apic = new Picture("images\\beach.jpg");
      Picture ferris1 = new Picture("images/2000 ferris wheel2.jpg");
@@ -37,25 +37,95 @@ public class TestPicture17
      Picture ferris3 = new Picture("images/2000 ferris wheel2.jpg");
 
      //apic.explore(); //DISPLAY PIC
-     ferris1.explore();
-     //moto.explore();
+     //ferris1.explore();
+     //moto.explore()
+     /**/
+     Picture snow = new Picture("images\\snowman.jpg");
+     Picture snow1 = new Picture("images\\snowman.jpg");
+     Picture snow2 = new Picture("images\\snowman.jpg");
+     Picture snow3 = new Picture("images\\snowman.jpg");
+     Picture snow4 = new Picture("images\\snowman.jpg");
+     snow.explore();
+     //changes color
+     Pixel[] Spixels;
+     Spixels = snow1.getPixels();
+     int red, blue, green;
+     for (Pixel spot1: Spixels)
+     {
+         red = spot1.getRed();
+         red = (int)(red * 2);
+         spot1.setRed(red);
+         
+         blue = spot1.getBlue();
+         blue = (int)(blue * 2);
+         spot1.setBlue(blue);
+         
+         green = spot1.getGreen();
+         green = (int)(green * 2);
+         spot1.setGreen(green);
+     }
+     snow1.explore();
+         
+     //makes it the opposite color, negates it
+     Spixels = snow2.getPixels();
+     for (Pixel spot1: Spixels)
+     {
+         red = spot1.getRed();
+         red = (int)(255 - red);
+         spot1.setRed(red);
+         
+         blue = spot1.getBlue();
+         blue = (int)(255- blue);
+         spot1.setBlue(blue);
+         
+         green = spot1.getGreen();
+         green = (int)(255 - green);
+         spot1.setGreen(green);
+     }
+     snow2.explore();
+     // makes it grayscale
+     Spixels = snow3.getPixels();
+     int avg;
+     for (Pixel spot1: Spixels)
+     {
+         red = spot1.getRed();
+         red = (int)(red * Math.random());
+         
+         blue = spot1.getBlue();
+         blue = (int)(blue * Math.random());
+         
+         green = spot1.getGreen();
+         green = (int)(green * Math.random());
+         
+         avg = (red+blue+green) / 3;
+         
+         spot1.setRed(avg);
+         spot1.setBlue(avg);
+         spot1.setGreen(avg);
+
+     }
+     snow3.explore();
+     
      
      
      //to change color of pic get all the pixels
      //makes an array of pixels
-     Pixel[] pixels;
+     //Pixel[] pixels;
      //gets pixels from picture and assigns to pixels array
      //need to repeat for every picture
-     pixels = ferris1.getPixels();
-    
+     //pixels = ferris1.getPixels();
+     
+     
+     //Pixel[] Mpixels;
+    // Mpixels = moto.getPixels();
      //how many pixels or how large array
-    System.out.println("This is a large array "+pixels.length  );
+    //System.out.println("This is a large array "+pixels.length  );
 
 
-    /**/
+    /*
         //access each index
     System.out.println(pixels[17]);
-    //access each pixel
+    //access each pixel 
     Pixel spot = ferris1.getPixel(100,100);
     Pixel spot2 = ferris1.getPixel(423,282);
     Pixel ferr17 = pixels[17];
@@ -78,7 +148,7 @@ public class TestPicture17
     
     for (int i = 0; i<10000; i++)
     {
-        Pixel yuck = ferris1.getPixel((int)(Math.random()*200),(int)(Math.random()*200));
+        Pixel yuck = ferris1.getPixel((int)(Math.random()*500),(int)(Math.random()*500));
         yuck.setColor(Color.green);
     }
     ferris1.explore();
@@ -92,10 +162,31 @@ public class TestPicture17
    // loop to access indexes of array or collection
 
     //for each loop spot  is a ?
-    for (Pixel spot : pixels)
-    System.out.println( spot );
+    int red;
+    for (Pixel spot1 : Mpixels)
+    {
+        //System.out.println( spot1 );
+        red = spot1.getRed();
+        
+        red = (int)(red * .25);
+    
+        spot1.setRed(red);
+    }
+    moto.explore();
 
-
+    int blue, green;
+    for (Pixel spot1: Mpixels)
+    {
+        blue = spot1.getBlue();
+        blue = (int)(blue * Math.random());
+        
+        green = spot1.getGreen();
+        green = (int)(green * Math.random());
+        
+        spot1.setBlue(blue);
+        spot1.setGreen(green);
+    }
+    moto.explore();
    
  /**/
 
@@ -143,7 +234,7 @@ final double  FACTOR = .5;
 
   /**/ 
     //write/save a picture as a file
-    ferris1.write("images/ferris11.jpg");
+    //ferris1.write("images/ferris11.jpg");
 
     /**/
   }//main
